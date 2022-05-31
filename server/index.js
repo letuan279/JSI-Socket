ecbda28d48e35e110820212f11f4a9ad1f7c5b3f
tuanle <latuan388@gmail.com>
2022-05-31 12:20:21 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    // ...
+    console.log("connection!!!");
+});
+
+httpServer.listen(3000);
