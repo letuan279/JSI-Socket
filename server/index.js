1b21effe995e249cc12754af3e79f77cfcdab3d0
tuanle <latuan388@gmail.com>
2022-06-01 18:00:24 +0700

    second commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+// packages
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
+// external file
+const router = require("./routes")
+
+
+// config server
 const app = express();
+const POST = 5000;
 app.use(cors());
 
 const httpServer = createServer(app);
@@ -14,9 +21,15 @@ const io = new Server(httpServer, {
     },
 });
 
+// router
+app.use('/', router);
+
+// start server
 io.on("connection", (socket) => {
     // ...
     console.log("connection!!!");
 });
 
-httpServer.listen(3000);
+httpServer.listen(POST, () => {
+    console.log(`Listening at port ${POST}`);
+});
